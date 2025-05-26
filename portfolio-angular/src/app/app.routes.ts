8526185aa62b77bd5e3a7f38f5fedade09623e41
Prateek Agarwal <prateekagarwal246@gmail.com>
2025-05-26 13:47:47 +0530

    routing

--- portfolio-angular/src/app/app.routes.ts
+++ portfolio-angular/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SkillsComponent } from './components/skills/skills.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'skills', component:SkillsComponent}
+];
