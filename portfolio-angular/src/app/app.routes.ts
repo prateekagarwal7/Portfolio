2728f54a4eb2af65cb11bfb0c072db34cf191725
Prateek Agarwal <prateekagarwal246@gmail.com>
2025-05-27 01:11:37 +0530

    form component

--- portfolio-angular/src/app/app.routes.ts
+++ portfolio-angular/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { SkillsComponent } from './components/skills/skills.component';
 import { WorkComponent } from './components/work/work.component';
+import { FormComponent } from './components/form/form.component';
 
 export const routes: Routes = [
     {path:'skills', component:SkillsComponent},
-    {path:'work',component:WorkComponent}
+    {path:'work',component:WorkComponent},
+    {path:'contact',component:FormComponent}
 ];
