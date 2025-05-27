455be34949ae09f8af76498650f66a8d85ddfaad
Prateek Agarwal <prateekagarwal246@gmail.com>
2025-05-27 23:26:03 +0530

    we have made the form to contact us and along wiht it we need to import the form module for ngmodel to work

--- portfolio-angular/src/app/components/form/form.component.ts
+++ portfolio-angular/src/app/components/form/form.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { TextareaModule } from 'primeng/textarea';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [CardModule,InputTextModule,TextareaModule,FloatLabelModule, ButtonModule, FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  first:string=''
 
 }
