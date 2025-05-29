5e6b27a910e8de484a75a1c1bdea7dd71ba5c018
Prateek Agarwal <prateekagarwal246@gmail.com>
2025-05-30 01:24:56 +0530

    routing changes for the home

--- portfolio-angular/src/app/app.routes.ts
+++ portfolio-angular/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { SkillsComponent } from './components/skills/skills.component';
 import { WorkComponent } from './components/work/work.component';
 import { FormComponent } from './components/form/form.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {path:'skills', component:SkillsComponent},
     {path:'work',component:WorkComponent},
-    {path:'contact',component:FormComponent}
+    {path:'contact',component:FormComponent},
+    {path:'home',component:HomeComponent}
 ];
