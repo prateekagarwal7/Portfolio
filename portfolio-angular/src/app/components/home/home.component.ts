ba36338ed93f365a89be11da7c82a3428c3a676c
Prateek Agarwal <prateekagarwal246@gmail.com>
2025-06-11 23:55:55 +0530

    using the envrionment variable for github

--- portfolio-angular/src/app/components/home/home.component.ts
+++ portfolio-angular/src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { github } from '../../../environments/environment.prod';
 @Component({
   selector: 'app-home',
   imports: [],
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+   git=github.url;
 }
