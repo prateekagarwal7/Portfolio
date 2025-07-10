54b25c089f866b3133eb6e7df333dcf94874998e
Prateek Agarwal <prateekagarwal246@gmail.com>
2025-07-10 23:54:20 +0530

    comminting changes for validating the form

--- portfolio-angular/src/app/components/form/form.component.ts
+++ portfolio-angular/src/app/components/form/form.component.ts
@@ -5,10 +5,11 @@ import { InputTextModule } from 'primeng/inputtext';
 import { TextareaModule } from 'primeng/textarea';
 import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-form',
-  imports: [CardModule,InputTextModule,TextareaModule,FloatLabelModule, ButtonModule, FormsModule],
+  imports: [CardModule,InputTextModule,TextareaModule,FloatLabelModule, ButtonModule, FormsModule, MessageModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
@@ -17,9 +18,52 @@ export class FormComponent {
   last:string='';
   phone:string='';
   email:string='';
-  msg:string=''
-  submitform(){
+  msg:string='';
+  errmsg:string='';
+  errcode:number=1;
 
+  
+  validateData()
+  {
+    if(this.first=='')
+    {
+      return {
+        message:"Please Enter valid Name",
+        code:0
+      }
+    }
+    if(this.last=='')
+    {
+      return {
+        message:"Please Enter valid Last Name",
+        code:0
+      }
+    }
+    if(this.email=='')
+    {
+      return {
+        message:"Please Enter valid Email",
+        code:0
+      }
+    }
+    if(this.phone.length==10)
+    {
+      return {
+        message:"Please Enter valid Phone Number Name",
+        code:0
+      }
+    }else{
+      return{
+        message:"Success",
+        code:1
+      }
+    }
+    
+  }
+  submitform(){
+    const res = this.validateData();
+    this.errmsg=res.message;
+    this.errcode=res.code;
   }
 
 }
